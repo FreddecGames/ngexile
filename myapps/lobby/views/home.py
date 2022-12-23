0dedb53a4f9f67b1c9eea911934a273fa2cd7346
Freddec <freddec.games@gmail.com>
2022-12-23 18:41:13 +0100

    dev

--- myapps/lobby/views/home.py
+++ myapps/lobby/views/home.py
@@ -2,8 +2,10 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views import View
 
+from socket import getaddrinfo, SOCK_STREAM
+
 
 class View(LoginRequiredMixin, View):
 
-    def get(self, request):
+    def get(self, request):    
         return render(request, 'lobby/home.html')
