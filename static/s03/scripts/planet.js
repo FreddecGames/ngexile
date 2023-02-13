7e7bc154651521817b717f291ebe54dbd4c2ce57
Freddec <freddec.games@gmail.com>
2023-02-13 22:12:38 +0100

    dev

--- static/s03/scripts/planet.js
+++ static/s03/scripts/planet.js
@@ -1,15 +1,15 @@
 function planet_str(id,name,g,s,p,rel)
 {
 	if(rel == 2)
-		var s = '<a href="/s03/planet/?planet='+id+'" class="self" data-bs-toggle="tooltip" data-bs-title="Voir planète">'+name+'</a> <a href="/s03/map/?g='+g+'&s='+s+'" class="ms-2 self" data-bs-toggle="tooltip" data-bs-title="Carte spatiale">'+g+'.'+s+'.'+p+'</a>';
+		var s = '<a href="/s03/planet/?planet='+id+'" class="text-self" data-bs-toggle="tooltip" data-bs-title="Voir planète">'+name+'</a> <a href="/s03/map/?g='+g+'&s='+s+'" class="ms-2 text-self" data-bs-toggle="tooltip" data-bs-title="Carte spatiale">'+g+'.'+s+'.'+p+'</a>';
 	else
 	{
 		switch(rel){
-			case 1: var col = 'ally'; break;
-			case 0:	var col = 'friend'; break;
-			case -1: var col = 'enemy'; break;
-			case -2: var col = 'enemy'; break;
-			case -3: var col = 'neutral'; break;
+			case 1: var col = 'text-success'; break;
+			case 0:	var col = 'text-friend'; break;
+			case -1: var col = 'text-danger'; break;
+			case -2: var col = 'text-danger'; break;
+			case -3: var col = 'text-normal'; break;
 		}
 
 		var s = '<a href="/s03/map/?g='+g+'&s='+s+'" class="'+col+'" data-bs-toggle="tooltip" data-bs-title="Carte spatiale">'+(name ? '<span class="me-2">' + name + '</span>': '')+'<span>'+g+'.'+s+'.'+p+'</span></a>';
