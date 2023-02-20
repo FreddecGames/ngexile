3288fb865cbbd069a9b464ee4b692bca8608aa5f
Freddec <freddec.games@gmail.com>
2023-02-20 10:11:18 +0100

    dev

--- myapps/s03/views/wait.py
+++ myapps/s03/views/wait.py
@@ -37,7 +37,7 @@ class View(ExileMixin, View):
 
         tpl = getTemplate(self.request, 's03/wait')
 
-        profile = dbRow('SELECT username, FROM users WHERE id=' + str(self.userId))
+        profile = dbRow('SELECT username FROM users WHERE id=' + str(self.userId))
         tpl.setValue('profile', profile)
 
         return render(self.request, tpl.template, tpl.data)
