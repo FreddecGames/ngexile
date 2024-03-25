6ad6e88b5325c9fcd4dd6c379830b71c48c61e85
Freddec <freddec.games@gmail.com>
2024-03-25 12:34:28 +0100

    dev

--- myapps/api/views.py
+++ myapps/api/views.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.db import connection
 from django.shortcuts import redirect
 
 from rest_framework.authentication import TokenAuthentication
@@ -6,6 +7,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+#---
 
 def reset_password(request, uidb64, token):
     base_url = settings.FRONT_END.get('BASE_URL')
@@ -20,13 +22,68 @@ def confirm_account(request, token):
 
     return redirect(f'{base_url}/{path}/{token}/')
 
+#---
 
-class Profile(APIView):
+def dict_fetchall(cursor):
+
+    columns = [col[0] for col in cursor.description]
+    return [
+        dict(zip(columns, row))
+        for row in cursor.fetchall()
+    ]
+
+def dict_fetchone(cursor):
+
+    results = dict_fetchall(cursor)
+    if results: return results[0]
+    else: return None
+
+cursor = None
+
+def dbConnect():
+
+    global cursor
+    cursor = connection.cursor()
+    
+def dbQuery(query):
+
+    cursor.execute(query)
+
+def dbRow(query):
+
+    cursor.execute(query)
+    return dict_fetchone(cursor)
+
+#---
+
+class BaseView(APIView):
     authentication_classes = [ TokenAuthentication ]
     permission_classes = [ IsAuthenticated ]
 
+    def initial(self, request, *args, **kwargs):
+        super(BaseView, self).initial(request, *args, **kwargs)
+        
+        self.userId = request.user.id
+        
+        dbConnect()
+
+        ipaddress = request.META.get('REMOTE_ADDR', '')
+        useragent = request.META.get('HTTP_USER_AGENT', '')
+        forwardedfor = request.META.get('HTTP_X_FORWARDED_FOR', '')
+        
+        dbQuery('SELECT * FROM sp_account_connect(' + str(self.userId) + ', 1036,' + dosql(ipaddress) + ',' + dosql(forwardedfor) + ',' + dosql(useragent) + ', 0)')
+
+
+class Profile(BaseView):
+
     def get(self, request, format=None):
         
-        data = {}
+        query = 'SELECT username, privilege, resets, credits, lastplanetid, deletion_date, planets, score, previous_score, mod_planets, mod_commanders,' + \
+                ' alliance_id, alliance_rank, leave_alliance_datetime IS NULL AND (alliance_left IS NULL OR alliance_left < now()) AS can_join_alliance,' + \
+                ' credits_bankruptcy, orientation, prestige_points' + \
+                ' FROM users' + \
+                ' WHERE id=' + str(self.userId)
+        
+        data = dbRow(query)
         return Response(data)
         
